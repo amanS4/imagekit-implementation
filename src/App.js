5a549ba1d384f6e5eb230cedb9691233d46e3596
amanS4 <amansingh4899@gmail.com>
2021-01-14 21:38:11 +0530

    ImageKit implimentation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Todos from './components/Todos.js';
+import Header from './components/layout/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    myArr: [
+      {
+        task: 'Cold Pressed Coconut Oil',
+        id: '1',
+        complete: false,
+        img1:
+          'https://ik.imagekit.io/l3zzzksiuty/gullack_products/product_1609423721_50044471712.webp',
+      },
+      {
+        task: 'Gentle Baby Wipes',
+        id: '2',
+        complete: false,
+        img1:
+          'https://ik.imagekit.io/l3zzzksiuty/gullack_products/product_1609416966_383452114.webp',
+      },
+      {
+        task: 'Baby Bedding Set with Mosquito Net',
+        id: '3',
+        complete: false,
+        img1:
+          'https://ik.imagekit.io/l3zzzksiuty/gullack_products/product_1575977347_1061135139.webp',
+      },
+    ],
+    urlEndpoint: 'https://ik.imagekit.io/l3zzzksiuty',
+  };
+
+  toggleComplete = (e) => {
+    this.setState({
+      myArr: this.state.myArr.map((obj) => {
+        if (obj.id === e) {
+          obj.complete = !obj.complete;
+        }
+        return obj;
+      }),
+    });
+  };
+  deletetask = (e) => {
+    this.setState({
+      myArr: [...this.state.myArr.filter((obj) => obj.id !== e)],
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Todos
+          myArr2={this.state.myArr}
+          toggleComplete={this.toggleComplete}
+          deletetask={this.deletetask}
+          urlEndpoint={this.state.urlEndpoint}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
